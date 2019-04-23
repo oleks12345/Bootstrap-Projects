87e371edbc682d722f9be1d78877965773a26eb1
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-04-23 16:06:22 +0200

    change navCheckTop() in app.js

--- Mache/app.js
+++ Mache/app.js
@@ -3,15 +3,12 @@ const navbar = document.querySelector('.navbar');
 const navbarToggler = document.querySelector('.navbar-toggler');
 
 const navCheckTop = () => {
-   setTimeout(() => {
-
-      if (window.pageYOffset > 0 || !navbarToggler.classList.contains('collapsed')) {
-         navbar.classList.add('navbar--top');
-      } // 
-      else {
-         navbar.classList.remove('navbar--top');
-      }
-   }, 0)
+   if (window.pageYOffset > 0) {
+      navbar.classList.add('navbar--top');
+   } // 
+   else {
+      navbar.classList.remove('navbar--top');
+   }
 };
 document.addEventListener('scroll', navCheckTop);
 navbar.addEventListener('click', navCheckTop);
